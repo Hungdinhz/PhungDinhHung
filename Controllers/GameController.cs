f0650d119775b70c296fbd4c531690f5d84b9c76
Hungdinhz <phunghung12112005@gmail.com>
2025-08-26 09:41:55 +0700

    push

---
+++ Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhungDinhHung.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Play()
+        {
+            return View();
+        }
+    }
+}
