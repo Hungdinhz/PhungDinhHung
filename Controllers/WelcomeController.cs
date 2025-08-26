4296ad7ad9ec1758969e52e2c9399c0197e08c58
Hungdinhz <phunghung12112005@gmail.com>
2025-08-26 09:31:41 +0700

    test

---
+++ Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhungDinhHung.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
